f6ee9bf0fa453064f4a1e6317b8fdd1f7d96d933
Sylv3str3 <sylvestre.mouly@gmail.com>
2014-08-07 10:18:36 +0000

    reduction du sleep a une seconde

--- templates/layout.php
+++ templates/layout.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>You shall past</title>
+    <title>You shall pass</title>
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
   </head>
